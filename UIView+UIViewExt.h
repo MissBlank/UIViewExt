9d288ef907cc18f88c97dd92671c4bfc961b9ea8
NERC <nerc@NERC.local>
2018-07-11 11:15:41 +0800

    new

---
+++ UIView+UIViewExt.h
@@ -0,0 +1,33 @@
+//
+//  UIView+UIViewExt.h
+//  UIViewext
+//
+//  Created by NERC on 2015/7/11.
+//  Copyright © 2015年 GaoNing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (UIViewExt)
+@property (nonatomic, assign) CGSize size;
+
+@property (nonatomic,assign)  CGFloat x;
+
+@property  (nonatomic,assign) CGFloat y;
+
+@property (nonatomic, assign) CGFloat top;
+
+@property (nonatomic, assign) CGFloat bottom;
+
+@property (nonatomic, assign) CGFloat left;
+
+@property (nonatomic, assign) CGFloat right;
+
+@property (nonatomic, assign) CGFloat centerX;
+
+@property (nonatomic, assign) CGFloat centerY;
+
+@property (nonatomic, assign) CGFloat width;
+
+@property (nonatomic, assign) CGFloat height;
+@end
